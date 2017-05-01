95147826dfee353f618efe11d2ee7c47065d7265
Leo <jamesleotucker@gmail.com>
2017-05-01 13:44:11 -0600

    added array to store Device IP info

--- src/client/frontPage/FrontPage.js
+++ src/client/frontPage/FrontPage.js
@@ -1,31 +1,73 @@
 import React from 'react';
 import axios from 'axios';
 
-const FrontPage = () => {   
-  async function asyncGetHospitals() {
-    const response = await axios.get('http://jsonplaceholder.typicode.com/posts');
-    console.log('Async Response: ', response);
+class FrontPage extends React.Component { 
+  constructor() {
+    super();
+    this.state = {
+      networkList: [],
+      devicesList: [],
+      counter: 0,
+      showProgress: false,
+    };
+    this.fetchNetworkInfo = this.fetchNetworkInfo.bind(this);
+    this.getDevices = this.getDevices.bind(this);
+    this.showProgress = this.showProgress.bind(this);
   }
-  function getHospitals() {
-    const response = axios.get('http://jsonplaceholder.typicode.com/posts');
-    console.log('Regular Function Response: ', response);
+
+  fetchNetworkInfo() {    
+    const orgId = 438518;
+    const networks = axios.get(`https://n155.meraki.com/api/v0/organizations/${orgId}/networks`, {     
+      headers: {
+        'X-Cisco-Meraki-API-Key': '	ab79946f614e1f04306533ae0dbf8f4fc59b2506',
+      },
+    }).then((response) => {
+      this.setState({ networkList: response.data });
+    }); 
   }
-  function promiseGetHospitals() {
-    axios.get('http://jsonplaceholder.typicode.com/posts')
-    .then((response) => 
-      console.log('Promise Response: ', response)
+
+  getDevices() {
+    axios.get(`https://n155.meraki.com/api/v0/networks/${this.state.networkList[this.state.counter].id}/devices`, {     
+      headers: {
+        'X-Cisco-Meraki-API-Key': '	ab79946f614e1f04306533ae0dbf8f4fc59b2506',      
+      },
+    }).then((response) => {       
+      const mxDevice = response.data.filter((device) => {       
+        /* Only return devices that have wan1Ip or Wan2Ip's as MX devices */
+        return (device.model && device.model.indexOf('MX') !== -1);                
+      });
+      let devicesList = this.state.devicesList;
+      let counter = this.state.counter;
+      devicesList.push(mxDevice);
+      counter++;
+      this.setState({ devicesList: devicesList, counter: counter });
+    });
+  }
+  showProgress() {
+    console.log(this.state.devicesList);
+    this.setState({ showProgress: true });
+  }
+  render() {
+    const prettyData = this.state.devicesList.map((site) => {
+      site.map((device) => {
+        return (
+          `<div>
+            <h5>${device.name}</h5>
+            <p>${device.wan1Ip}</p>
+            <p>${device.wan2Ip}</p>
+          </div>`
+        );
+      });
+    });
+    return (
+        <div>
+            <button onClick={this.fetchNetworkInfo}>Get Network list!</button>
+            <button onClick={this.getDevices}>Push MX Devices!</button>
+            <button onClick={this.showProgress}>Summarize in console</button>
+            <div>{this.state.showProgress ? prettyData : '' }</div>
+        </div>
     );
   }
-  function callAllFunctions() {
-    getHospitals();
-    promiseGetHospitals();
-    asyncGetHospitals();
-  }
-  return (
-      <div>
-          <a onClick={callAllFunctions}>Click me!</a>
-      </div>
-  );
-};
+}
 
 export default FrontPage;
